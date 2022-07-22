daab7e450584779958bf2749d6e9397196494872
KeDuCa96 <stevanduran96@gmail.com>
2022-07-21 23:22:02 -0500

    Add composer, NPM and routings

--- public/index.php
+++ public/index.php
@@ -2,9 +2,22 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
+        //Login and logout
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
+    //Recover password
+$router->get('/forget', [LoginController::class, 'forget']);
+$router->post('/forget', [LoginController::class, 'forget']);
+$router->get('/recover', [LoginController::class, 'recover']);
+$router->post('/recover', [LoginController::class, 'recover']);
+    //Crete account
+$router->get('/create', [LoginController::class, 'create']);
+$router->post('/create', [LoginController::class, 'create']);
 
 
 
