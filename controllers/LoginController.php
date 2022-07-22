133bc48c181ccaeb15127a8d9f9be75b6662dee9
KeDuCa96 <stevanduran96@gmail.com>
2022-07-21 23:27:08 -0500

    Add composer, NPM and routings

---
+++ controllers/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+namespace Controllers;
+
+class LoginController{
+    public static function login() {
+        echo "from login";
+    }
+
+    public static function logout() {
+        echo "from logout";
+    }
+    public static function forget() {
+        echo "from forget";
+    }
+    public static function recover() {
+        echo "from recover";
+    }
+    public static function create() {
+        echo "from create";
+    }
+}
--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', '', '');
+$db = mysqli_connect('localhost', 'root', '1234', 'app-tattoo');
 
 
 if (!$db) {
